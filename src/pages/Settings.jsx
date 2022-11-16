cb09cf50146721d2f4ba67c82b795bb1ea406856
Lucas Rocha <ld.rocha@hotmail.com>
2022-11-16 01:23:28 -0800

    implement settings options

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,3 +1,63 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Stack,
+  Box,
+  Heading,
+  StackDivider,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+
+import { NavigationBar } from "../components/NavigationBar";
+
 export function Settings() {
-  return <h1>Settings</h1>;
+  return (
+    <>
+      <NavigationBar />
+      <Heading m="50px" textAlign="center">
+        Settings
+      </Heading>
+      <Card bgColor="white" margin="0 auto" w="50%">
+        <CardHeader>
+          <Heading size="md">User Settings</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing="4">
+            <Box>
+              <Button
+                variant="link"
+                colorScheme="purple"
+                _hover={{ color: "#5149fb" }}
+              >
+                Edit User Information
+              </Button>
+              <Text pt="2" fontSize="sm">
+                Edit user information such as name, e-mail and profile picture
+              </Text>
+            </Box>
+            <Box>
+              <Button
+                variant="link"
+                colorScheme="purple"
+                _hover={{ color: "#5149fb" }}
+              >
+                Change Currency
+              </Button>
+              <Text pt="2" fontSize="sm">
+                Choose a different currency
+              </Text>
+            </Box>
+            <Box>
+              <Button colorScheme="purple" _hover={{ bg: "#5149fb" }}>
+                Logout
+              </Button>
+            </Box>
+          </Stack>
+        </CardBody>
+      </Card>
+    </>
+  );
 }
